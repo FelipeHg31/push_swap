1d4bdc64c9d9bc1675903f80cef6de37bf663378
FelipeHg31 <113516848+FelipeHg31@users.noreply.github.com>
2025-11-07 23:46:39 +0100

    funk tribu

---
+++ push_swap/libft/ft_lstadd_front_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan-her <juan-her@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 02:48:08 by juan-her          #+#    #+#             */
+/*   Updated: 2025/11/07 21:17:53 by juan-her         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	if (*lst)
+		(*lst)->before = new;
+	*lst = new;
+	new->before = NULL;
+}
---
+++ push_swap/movements.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movements.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan-her <juan-her@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 18:01:41 by juan-her          #+#    #+#             */
+/*   Updated: 2025/11/07 21:35:45 by juan-her         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MOVEMENTS_H
+# define MOVEMENTS_H
+# include "ft_printf/src/ft_printf.h"
+# include "libft/libft.h"
+
+int		ft_swap(t_list **list);
+void	ft_pb(t_data **data);
+void	ft_pa(t_data **data);
+
+#endif
