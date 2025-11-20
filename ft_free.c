4807f65780d2c89a5978329cdc5a8832673e732f
Juan Felipe Hernandez Garcia <juan-her@c2r9s4.42madrid.com>
2025-11-20 14:14:44 +0100

    g

--- ft_free.c
+++ ft_free.c
@@ -6,7 +6,7 @@
 /*   By: juan-her <juan-her@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 12:23:59 by juan-her          #+#    #+#             */
-/*   Updated: 2025/11/20 00:31:29 by juan-her         ###   ########.fr       */
+/*   Updated: 2025/11/20 14:14:18 by juan-her         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	ft_free(t_data **stacks)
 int	ft_free_error(t_data **stacks)
 {
 	ft_free(stacks);
-	write(2, "Error.", 6);
+	write(2, "Error", 5);
 	return (1);
 }
